b47d5966840f32c7a549b797fc8f53ecc5fb63e8
Ayabablanca <133345081+Ayabablanca@users.noreply.github.com>
2023-07-19 21:43:25 +0200

    0-print_name.c

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,8 +1,8 @@
-#ifndef _HEADER_
-#define _HEADER_
-#include <stdlib.h>
+#ifndef MAIN_READER_FILE
+#define MAIN_READER_FILE
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
